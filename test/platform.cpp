3c4c350c956a6e68f81b77717723cc0e0ebfa161
Harikrishnan <harikrishnan.prabakaran@gmail.com>
2023-02-21 11:09:06 +0000

    Use inline assembly to fix bug in syscall

--- test/platform.cpp
+++ test/platform.cpp
@@ -2,45 +2,58 @@
 
 extern "C" auto main() -> int;
 
-static auto syscall(
-    [[maybe_unused]] long call_no,
-    [[maybe_unused]] long arg1 = 0,
-    [[maybe_unused]] long arg2 = 0,
-    [[maybe_unused]] long arg3 = 0,
-    [[maybe_unused]] long arg4 = 0,
-    [[maybe_unused]] long arg5 = 0,
-    [[maybe_unused]] long arg6 = 0,
-    [[maybe_unused]] long arg7 = 0,
-    [[maybe_unused]] long arg8 = 0) -> long {
+template<long CALL_NO>
+static auto
+syscall(long arg1 = 0, long arg2 = 0, long arg3 = 0, long arg4 = 0, long arg5 = 0, long arg6 = 0)
+    -> long {
+    long ret = 0;
+
 #if ARCH_IS_X86_64 == 1
     asm volatile(
-        "movq %rdi, %rax\n"
-        "movq %rsi, %rdi\n"
-        "movq %rdx, %rsi\n"
-        "movq %rcx, %rdx\n"
-        "movq %r8, %r10\n"
-        "movq %r9, %r8\n"
-        "movq 8(%rsp), %r9\n"
+        "mov %[call_no], %%rax\n"
+        "mov %[arg1], %%rdi\n"
+        "mov %[arg2], %%rsi\n"
+        "mov %[arg3], %%rdx\n"
+        "mov %[arg4], %%r10\n"
+        "mov %[arg5], %%r8\n"
+        "mov %[arg6], %%r9\n"
         "syscall\n"
-        "ret\n");
+        "mov %%rax, %[ret]\n"
+        : [ret] "=r"(ret)
+        : [call_no] "i"(CALL_NO),
+          [arg1] "r"(arg1),
+          [arg2] "r"(arg2),
+          [arg3] "r"(arg3),
+          [arg4] "r"(arg4),
+          [arg5] "r"(arg5),
+          [arg6] "r"(arg6)
+        : "rax", "rdi", "rsi", "rdx", "r10", "r8", "r9");
 #elif ARCH_IS_AARCH64 == 1
     asm volatile(
-        "mov x8, x0\n"
-        "mov x0, x1\n"
-        "mov x1, x2\n"
-        "mov x2, x3\n"
-        "mov x3, x4\n"
-        "mov x4, x5\n"
-        "mov x5, x6\n"
-        "mov x6, x7\n"
+        "mov x8, %[call_no]\n"
+        "mov x0, %[arg1]\n"
+        "mov x1, %[arg2]\n"
+        "mov x2, %[arg3]\n"
+        "mov x3, %[arg4]\n"
+        "mov x4, %[arg5]\n"
+        "mov x5, %[arg6]\n"
         "svc #0\n"
-        "ret\n");
+        : [ret] "=r"(ret)
+        : [call_no] "i"(CALL_NO),
+          [arg1] "r"(arg1),
+          [arg2] "r"(arg2),
+          [arg3] "r"(arg3),
+          [arg4] "r"(arg4),
+          [arg5] "r"(arg5),
+          [arg6] "r"(arg6)
+        : "x0", "x1", "x2", "x3", "x4", "x5", "x8");
 #endif
-    __builtin_unreachable();
+
+    return ret;
 }
 
 [[noreturn]] static void exit(int ret) {
-    syscall(SYS_exit, ret);
+    syscall<SYS_exit>(ret);
     __builtin_unreachable();
 }
 
