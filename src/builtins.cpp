6e80b81600e800758fbf27bcb6e8b1b28290e647
Harikrishnan <harikrishnan.prabakaran@gmail.com>
2023-02-21 15:02:01 +0530

    Provide platform (startup and exit) for tests (#5)

---
+++ src/builtins.cpp
@@ -0,0 +1,23 @@
+#include <cstddef>
+
+extern "C" void memcpy(void* __restrict__ dst_p, const void* __restrict__ src_p, size_t size) {
+    auto* dst = static_cast<char*>(dst_p);
+    const auto* src = static_cast<const char*>(src_p);
+
+    for (size_t i = 0; i < size; i++) {
+        dst[i] = src[i];
+    }
+}
+
+extern "C" void memmove(void* __restrict__ dst_p, const void* __restrict__ src_p, size_t size) {
+    auto* dst = static_cast<char*>(dst_p);
+    const auto* src = static_cast<const char*>(src_p);
+}
+
+extern "C" void memset(void* __restrict__ dst_p, int val, size_t size) {
+    auto* dst = static_cast<char*>(dst_p);
+
+    for (size_t i = 0; i < size; i++) {
+        dst[i] = static_cast<char>(val);
+    }
+}
