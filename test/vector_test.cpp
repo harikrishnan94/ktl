eaea02ac0bdc6edb508f4890cd797ee420fb2559
Harikrishnan <harikrishnan.prabakaran@gmail.com>
2023-03-28 14:18:16 +0530

    Move InputItterator out of vector_test -> to be used by other tests (#37)

--- test/vector_test.cpp
+++ test/vector_test.cpp
@@ -2,39 +2,9 @@
 #include <ktl/span.hpp>
 #include <ktl/stack_vector.hpp>
 
+#include "input_iterator.hpp"
+
 using namespace ktl;
-
-template<std::input_iterator It>
-class InputIterator {
-  public:
-    using value_type = std::iter_value_t<It>;
-    using difference_type = std::iter_difference_t<It>;
-    using reference = std::iter_reference_t<It>;
-
-    constexpr explicit InputIterator(It it) : m_it(it) {}
-
-    constexpr auto operator*() const noexcept -> reference {
-        return *m_it;
-    }
-    constexpr auto operator->() const noexcept -> value_type* {
-        return &*m_it;
-    }
-
-    constexpr auto operator++() noexcept -> InputIterator& {
-        ++m_it;
-        return *this;
-    }
-    constexpr auto operator++(int) noexcept -> InputIterator {
-        auto copy = *this;
-        ++*this;
-        return copy;
-    }
-
-    constexpr auto operator<=>(const InputIterator& o) const = default;
-
-  private:
-    It m_it;
-};
 
 struct int_t {
     int_t() = default;
